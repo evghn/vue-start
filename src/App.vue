d16ee0d0bfa1d0f8c41196c1b744f7f693360b4e
evgenii.kharin <evgenii.kharin@tel-core.ru>
2026-02-09 20:41:54 +0300

    обработка ошибок

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script setup>
-import { computed, ref, watch } from 'vue';
+import { computed, ref, watch, watchEffect } from 'vue';
 
 const hello = "hello user";
-const data = ref()
+const task = ref()
 const _dataList = ref([])
 const filter = ref(false)
+const taskLevel = ref()
+const errorTask = ref(false)
+const errorLevel = ref(false)
+const errorTaskMessage = ref("Не заполнено название задачи")
+const errorLevelMessage = ref("Не выбран уровень задачи")
+
+const level = ref([
+  { id: 0, text: '' },
+  { id: 1, text: 'Легкая' },
+  { id: 2, text: 'Средняя' },
+  { id: 3, text: 'Сложная' }
+])
+
+
 
 const count = computed(() => dataList.value.length)
 
@@ -19,17 +33,45 @@ const clearTasks = () => {
 }
 
 const addTask = () => {
+  if (!task.value || !task.value.length) {
+    errorTask.value = true
+  }
+
+  if (!taskLevel.value) {
+    errorLevel.value = true
+  }
+
+  if (errorTask.value || errorLevel.value) {
+    return
+  }
+
+
+
+
   _dataList.value.push({
     id: Date.now(),
-    value: data.value,
+    value: task.value,
+    level: "low",
     status: "active"
   })
-  console.log(data.value);
-  data.value = "";
+  console.log(task.value);
+  task.value = "";
 }
 
 
-watch(_dataList.value,
+const handleTask = (e) => {
+  // console.log(errorTask.value, e.key);
+
+  if (errorTask.value) {
+    errorTask.value = false;
+  }
+
+  if (e.key == "Enter") {
+    addTask()
+  }
+}
+
+watch(() => _dataList.value,
   (newVal) => {
     if (newVal)
       console.log("dataList изменен", newVal);
@@ -40,6 +82,16 @@ watch(_dataList.value,
   }
 )
 
+watchEffect(
+  () => {
+    console.log("errorLevel", errorLevel.value);
+    if (errorLevel.value && taskLevel.value) {
+      errorLevel.value = false
+    }
+    // console.log(task.value);
+  }
+)
+
 </script>
 
 <template>
@@ -47,13 +99,33 @@ watch(_dataList.value,
     {{ hello }}
     <div class="flex flex-col gap-3">
 
-      <div class="flex gap-3 justify-between">
-        <input class="border px-4 text-2xl rounded w-[90%] active:border  active:border-gray-200" type="text"
-          v-model.number="data" @keydown.enter="addTask">
-        <a class="border px-4 py-2 rounded-lg bg-green-600   text-white min-w-40 tetx-center" href=""
-          @click.prevent="addTask">Добавить
-          задачу</a>
+      <div class="flex flex-col gap-3">
+
+        <div class="flex gap-3 justify-between">
+          <input class="border px-4 text-2xl rounded w-[70%] active:border  active:border-gray-200" type="text"
+            v-model="task" @keydown="handleTask">
+
+          <div class="flex gap-3 min-w-60 items-end">
+            <div>Сложность задачи</div>
+            <select v-model="taskLevel" class="border border-gray-400 p-2 rounded">
+              <option v-for="item in level" :key="item.id" :value="item.id">{{ item.text }}</option>
+            </select>
+
+          </div>
+          <a class="border px-4 py-2 rounded-lg bg-green-600   text-white min-w-40 tetx-center" href=""
+            @click.prevent="addTask">Добавить задачу</a>
+        </div>
+        <div v-if="errorTask || errorLevel" class="bg-red-600 text-white px-3 py-2">
+          <div v-if="errorTask">
+            {{ errorTaskMessage }}
+          </div>
+          <div v-if="errorLevel">
+            {{ errorLevelMessage }}
+          </div>
+        </div>
       </div>
+
+
       <div class="flex gap-3 justify-between border-blue-900 border p-3 rounded">
         <div><a href="" @click.prevent="filter = !filter">filter</a></div>
         <div class="flex gap-3 justify-between">
